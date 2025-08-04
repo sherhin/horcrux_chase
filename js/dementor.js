bb3888b43cb203d80073ea2fbc5812cc4caea182
Tatiana Evmenova <t.evmenova@ntechlab.com>
2025-08-04 23:40:53 +0300

    fix typo

---
+++ js/dementor.js
@@ -0,0 +1,22 @@
+let dementors = [];
+
+export function generateDementors(template, map) {
+  dementors = [];
+  let count = 0;
+
+  while (count < 3) {
+    let x = Math.floor(Math.random() * map[0].length);
+    let y = Math.floor(Math.random() * map.length);
+
+    if (map[y][x] === 0 && !dementors.some(d => d.x === x && d.y === y)) {
+      dementors.push({ image: template, x, y });
+      count++;
+    }
+  }
+}
+
+export function drawDementors(ctx, tileSize) {
+  dementors.forEach(d => {
+    ctx.drawImage(d.image, d.x * tileSize, d.y * tileSize, tileSize, tileSize);
+  });
+}
